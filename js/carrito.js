d20a858e776d002e0c6b3fb86671ffd30715712b
Lucas <lcasagrande@itba.edu.ar>
2015-10-01 12:40:07 -0300

    agregue las liberias de angular. ahora los dropdown se actualizan con el dato que elegis (no se como funciona, pero funciona)

---
+++ js/carrito.js
@@ -0,0 +1,23 @@
+(function() {
+var app = angular.module('Carrito', []);
+ app.controller('TalleController',function(){
+ 	this.talles = talles;
+ 	this.setTalle = function(talle){
+ 		
+ 	}
+ });
+ app.controller('ColorController',function($scope){
+ 	this.colores = colores;
+ 	this.setColor = function(talle){
+ 		$scope.colordd = talle;
+ 	}
+ });
+
+ var talles = ['S','M','L','XL'];
+ var colores = ['Amarillo','Verde','Azul','Rojo'];
+})();
+
+ $(".dropdown-menu").on('click', 'li a', function(){
+        $(this).parent().parent().siblings(".btn:first-child").html($(this).text()+' <span class="caret"></span>');
+        $(this).parent().parent().siblings(".btn:first-child").val($(this).text());
+    });
