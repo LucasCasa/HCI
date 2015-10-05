a9ae97ff6e1aeb09a2f983a9a9f244670c91f062
LucasCasa <lcasagrande@itba.edu.ar>
2015-10-05 19:18:56 -0300

    cambios varios

--- js/carrito.js
+++ js/carrito.js
@@ -6,13 +6,6 @@ var app = angular.module('Carrito', []);
  		
  	}
  });
-
-	app.directive('navBar',function{
-	return{
-		restrict: 'E',
-		templateUrl : "nav.html"
-	};
-	}
  app.controller('ColorController',function($scope){
  	this.colores = colores;
  	this.setColor = function(talle){
