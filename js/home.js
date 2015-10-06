d60f1b13beec85655611c85a9475ffdd12ab37c8
Lucas <lcasagrande@itba.edu.ar>
2015-10-06 08:07:12 -0300

    Kuyum no es el unico que no puede dormir si las cosas no estan bien hechas. ahora en la home en los destacados aparecen alpargatas

---
+++ js/home.js
@@ -0,0 +1,11 @@
+(function(){
+	var app = angular.module('home',[]);
+	
+
+app.controller('DestacadosController',function($scope,$http,$log){	
+	$http.get("http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetProductsByCategoryId&id=1").then(function(res){
+		$scope.destacados = res.data.products;
+	});
+});
+	
+})();
