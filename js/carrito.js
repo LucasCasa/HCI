f7fc9fa2a1bc74a4cf08c7161305357d27d7365b
Lucas <lcasagrande@itba.edu.ar>
2015-10-07 17:14:33 -0300

    modifique el carrito ahora aparecen muchos productos y la barra de al lado muestra un resumen, que tipo inteligente que soy

--- js/carrito.js
+++ js/carrito.js
@@ -19,6 +19,16 @@ var app = angular.module('Carrito', []);
  	};
 
  });
+ app.controller('CarritoController',function($scope,$http,$log){	
+	$http.get("http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetProductsByCategoryId&id=3").then(function(res){
+		$scope.productos = res.data.products;
+		$scope.total = 0;
+		res.data.products.forEach(function(entry){
+			$scope.total+= entry.price;
+		})
+
+	});
+});
 
  var talles = ['S','M','L','XL'];
  var colores = ['Amarillo','Verde','Azul','Rojo'];
