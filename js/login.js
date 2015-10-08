725e3eecec9ea1b7e6b2e6971e6147f429bae211
nicokuyum <nicokuyum@hotmail.com>
2015-10-08 17:23:05 -0300

    El register ahora funciona como en el prototipo, aparece cuando se clickea un boton.  Ademas se volvio a agregar el estilo del footer y se lo separó a otro archivo

--- js/login.js
+++ js/login.js
@@ -2,9 +2,20 @@
 var app = angular.module('login',[]);
 
 app.controller('RegisterController',function($scope,$http,$log){
-
+	this.isRegistering = false;
+	this.toggleRegistering = function(){
+		this.isRegistering = !this.isRegistering;
+	};
+	this.getInclude = function(){
+		if(this.isRegistering){
+			return 'register.html';
+		}else{
+			return '';
+		}
+	}
 });
- app.directive('navBar',function(){
+
+app.directive('navBar',function(){
  	return{
  		restrict: 'E',
  		templateUrl: "nav.html"
@@ -12,14 +23,12 @@ app.controller('RegisterController',function($scope,$http,$log){
 
  });
 app.controller("LoginController",function($scope,$http,$log){
-
-
-$scope.login = function(){
-	$http.get("http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=SignIn&username=" + $scope.loginUser + "&password=" + $scope.loginPass).then(function(res){
-		$log.debug("Estas logueado, tu token es: " + res.data.authenticationToken);
-		$log.debug(res);
-	});
-};
-
+	$scope.login = function(){
+		$http.get("http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=SignIn&username=" + $scope.loginUser + "&password=" + $scope.loginPass).then(function(res){
+			$log.debug("Estas logueado, tu token es: " + res.data.authenticationToken);
+			$log.debug(res);
+		});
+	};
 })
+
 })();
