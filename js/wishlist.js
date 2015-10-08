59624c53ac272ddd06ac7a5f71bb40b7165c6a55
Lucas <lcasagrande@itba.edu.ar>
2015-10-07 21:52:13 -0300

    ahora se muestran productos en la wishlist, finalizar carrito y el carrito muestra resumen de productos

--- js/wishlist.js
+++ js/wishlist.js
@@ -1,5 +1,39 @@
+(function(){ 
+var app = angular.module('wishlist',[]);
+
+ app.controller('WishlistController',function($scope,$http,$log){	
+	$http.get("http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetProductsByCategoryId&id=2").then(function(res){
+		$scope.productos = res.data.products;
+	});
+});
+ app.controller('TalleController',function(){
+ 	this.talles = talles;
+ 	this.setTalle = function(talle){
+ 		
+ 	}
+ });
+ app.controller('ColorController',function($scope){
+ 	this.colores = colores;
+ 	this.setColor = function(talle){
+ 		$scope.colordd = talle;
+ 	}
+ });
+ app.directive('navBar',function(){
+ 	return{
+ 		restrict: 'E',
+ 		templateUrl: "nav.html"
+ 	};
+
+ });
+ var talles = ['S','M','L','XL'];
+ var colores = ['Amarillo','Verde','Azul','Rojo'];
+})();
+
 $(document).ready(function(){
 	$("tbody").do("click",".btn-rmv",function(){
 		$(this).closest("tr").remove();
 	});
+
+
 });
+
