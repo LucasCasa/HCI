3e4120ddcf9ba54bae1d2832a4797c8d77861cc6
Lucas <lcasagrande@itba.edu.ar>
2015-10-10 13:29:55 -0300

    solucionado lo de producto

--- js/producto.js
+++ js/producto.js
@@ -1,5 +1,5 @@
 (function(){
-var app = angular.module('productoApp',['navBar']);
+var app = angular.module('productoApp',['navbar']);
 
 app.directive('navBar',function(){
  	return{
@@ -8,7 +8,6 @@ app.directive('navBar',function(){
  	};
 
  });
-var producto = {};
 app.controller('ProductController',function($scope,$http,$log){
 	var prodId = parent.document.URL.substring(parent.document.URL.indexOf('?') + 4, parent.document.URL.length);
 	$http.get("http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetProductById&id=" + prodId).then(function(res){
@@ -17,9 +16,24 @@ app.controller('ProductController',function($scope,$http,$log){
 		$log.debug(prodId);
 	});
 });
+ app.controller('TalleController',function(){
+ 	this.talles = talles;
+ 	this.setTalle = function(talle){
+ 		
+ 	}
+ });
+ app.controller('ColorController',function($scope){
+ 	this.colores = colores;
+ 	this.setColor = function(talle){
+ 		$scope.colordd = talle;
+ 	}
+ });
 
+ var talles = ['S','M','L','XL'];
+ var colores = ['Amarillo','Verde','Azul','Rojo'];
 app.controller('ReviewController',function($scope,$http,$log){
 	this.variable = 2;
+	$log.debug("hola");
 });
 
 })();
