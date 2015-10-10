f761125cc15a04873760bd673894cbd8bbff100e
Nico <nicokuyum@hotmail.com>
2015-10-10 13:09:35 -0300

    Test para ver como rompo todo

--- js/producto.js
+++ js/producto.js
@@ -1,5 +1,5 @@
 (function(){
-var app = angular.module('productoApp',[]);
+var app = angular.module('productoApp',['navBar']);
 
 app.directive('navBar',function(){
  	return{
@@ -10,7 +10,7 @@ app.directive('navBar',function(){
  });
 var producto = {};
 app.controller('ProductController',function($scope,$http,$log){
-var prodId = parent.document.URL.substring(parent.document.URL.indexOf('?') + 4, parent.document.URL.length);
+	var prodId = parent.document.URL.substring(parent.document.URL.indexOf('?') + 4, parent.document.URL.length);
 	$http.get("http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetProductById&id=" + prodId).then(function(res){
 		$scope.producto = res.data.product;
 		$log.debug(res);
@@ -18,5 +18,8 @@ var prodId = parent.document.URL.substring(parent.document.URL.indexOf('?') + 4,
 	});
 });
 
+app.controller('ReviewController',function($scope,$http,$log){
+	this.variable = 2;
+});
 
 })();
