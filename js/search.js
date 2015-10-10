c741d72d805c10956ca80621d5d1cc8e7dc18aed
derowon <derowon@gmail.com>
2015-10-11 05:21:06 +0900

    Busca por nombre, pero falta todavia

--- js/search.js
+++ js/search.js
@@ -2,9 +2,43 @@
 var app = angular.module('search', ['navbar']);
 
 
-app.controller('BusquedaController',function($scope,$http,$log){
-	$http.get("http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetProductsByCategoryId&id=1").then(function(res){
+
+app.controller('BusquedaController',function($scope,$http,$log,$location){
+
+	//$locationProvider.html5Mode(true);
+
+
+	 $scope.loading = true;
+		var onSuccess = function(res){
 		$scope.destacados = res.data.products;
-	});
-});
+		$scope.loading = false;
+
+		}
+
+	var onErrorOcurred= function(res){
+		$scope.error = "Error";
+	}
+	var url = window.location.href;
+	console.log(url);
+	var urlArray = url.split("/");
+	var pos = urlArray.length;
+
+	url = urlArray[pos-1];
+	console.log(url);
+
+	var param = (url.split("?")[1]).split("&");
+
+	var nombre = param[0].split("=")[1];
+	var categ = param[1].split("=")[1];
+
+	console.log(nombre);
+	console.log(categ);
+
+
+
+	$http.get("http://eiffel.itba.edu.ar/hci/service3/Catalog.groovy?method=GetProductsByName&name="+nombre).then(onSuccess,onErrorOcurred);
+
+
+})
+
 })();
