aff4fa973c8a06025843e5310ee9099601ae2bee
Lucas <lcasagrande@itba.edu.ar>
2015-10-12 19:52:32 -0300

    ahora funciona la validacion de usuario. el unico problema seria si hay repetidos, pero es de la api y tengo que ver cmo lo resuelvo.

--- js/login.js
+++ js/login.js
@@ -24,55 +24,82 @@ app.controller('RegisterController',function($scope,$http,$log){
 
 
 	this.dayValidator = function(){
-		if(this.DOBDay.length == 1){
-			if(isPositiveInteger(this.DOBDay)){
-				if(this.DOBDay > 4){
-					this.DOBDay = '0' + this.DOBDay;
-					$('#month').focus();
+		if(this.DOBDay !== undefined){
+			if(this.DOBDay.length == 1){
+				if(isPositiveInteger(this.DOBDay)){
+					if(this.DOBDay > 3){
+						this.DOBDay = '0' + this.DOBDay;
+						$('#month').focus();
+					}
+				}else{
+					this.DOBDay=last;
 				}
-			}else{
-				this.DOBDay=last;
-			}
-		}else if( this.DOBDay.length >= 2){
-			if(isPositiveInteger(this.DOBDay) && this.DOBDay <= 31){
-				$('#month').focus();
-			}else{
-				this.DOBDay = last;
-			}
+			}else if( this.DOBDay.length >= 2){
+				if(isPositiveInteger(this.DOBDay) && this.DOBDay <= 31){
+					$('#month').focus();
+				}else{
+					this.DOBDay = last;
+				}
 
+			}
 		}
 		last = this.DOBDay;
 	}
 
 	this.monthValidator = function(){
-		if(this.DOBMonth.length == 1){
-			if(isPositiveInteger(this.DOBMonth)){
-				if(this.DOBMonth > 1){
-					this.DOBMonth = '0' + this.DOBMonth;
-					$('#year').focus();
+		if(this.DOBMonth !== undefined){
+			if(this.DOBMonth.length == 1){
+				if(isPositiveInteger(this.DOBMonth)){
+					if(this.DOBMonth > 1){
+						this.DOBMonth = '0' + this.DOBMonth;
+						$('#year').focus();
+					}
+				}else{
+					this.DOBMonth=lastM;
 				}
-			}else{
-				this.DOBMonth=lastM;
-			}
-		}else if( this.DOBMonth.length >= 2){
-			if(isPositiveInteger(this.DOBMonth) && this.DOBMonth <= 12){
-				$('#year').focus();
-			}else{
-				this.DOBMonth = lastM;
-			}
+			}else if( this.DOBMonth.length >= 2){
+				if(isPositiveInteger(this.DOBMonth) && this.DOBMonth <= 12){
+					$('#year').focus();
+				}else{
+					this.DOBMonth = lastM;
+				}
 
+			}
 		}
 		lastM = this.DOBMonth;
 	}
 
 	this.yearValidator = function(){
-		if(this.DOBYear.length != 0 && (!isPositiveInteger(this.DOBYear) || this.DOBYear > 2000) ){
+		if(this.DOBYear !== undefined && (!isPositiveInteger(this.DOBYear) || this.DOBYear > 2000) ){
 			this.DOBYear = last;
 		}
 		last = this.DOBYear;
 	}
 
-
+	this.isValid = function(){
+		if(this.username === undefined || this.username.length < 6 || this.username.length > 15){
+			return false;
+		}
+		if(this.pass1 !== this.pass2 || this.pass1 === undefined || this.pass1.length < 8 || this.pass1.length > 15){
+			return false;
+		}
+		if(this.firstname === undefined || this.firstname.length > 80 || this.lastname === undefined || this.lastname.length > 80){
+			return false;
+		}
+		if(this.gender !== 'M' && this.gender !== 'F'){
+			return false;
+		}
+		if(!isPositiveInteger(this.identityCard) || this.identityCard.length > 10){
+			return false;
+		}
+		if(this.email === undefined || this.email.indexOf('@') == -1 || this.email.indexOf('.') == -1 ||  this.email.lastIndexOf('.') - this.email.indexOf('@') < 2){
+			return false;
+		}
+		if(this.DOBDay === undefined || this.DOBMonth === undefined || this.DOBYear === undefined || this.DOBYear < 1900){
+			return false;
+		}
+		return true;
+	}
 });
 
 app.directive('navBar',function(){
@@ -104,6 +131,7 @@ app.controller("LoginController",function($scope,$http,$log){
 		});
 	};
 });
+
 
 })();
 	function isPositiveInteger(n) {
