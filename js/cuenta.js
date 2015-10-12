85dd8fc140a72370db4ed95e0026c8e89f57d553
Lucas <lcasagrande@itba.edu.ar>
2015-10-12 12:20:40 -0300

    le saque los campos inncesarios a direccion e hice que lo cargara desde el archivo externo

--- js/cuenta.js
+++ js/cuenta.js
@@ -7,6 +7,13 @@ var app = angular.module('Cuenta', ['navbar']);
  	};
 
  });
+  app.directive('addAddress',function(){
+ 	return{
+ 		restrict: 'E',
+ 		templateUrl: "add-address.html"
+ 	};
+
+ });
  app.controller("CuentaController",function($http,$log,$scope){
  	var cookie = document.cookie;
  	var user = cookie.substring(cookie.indexOf('user=')+5,cookie.length);
@@ -25,6 +32,7 @@ var app = angular.module('Cuenta', ['navbar']);
  		return ($scope.direcciones == undefined || $scope.direcciones.length == 0);
  	}
  	$scope.saveAddress = function(){
+ 		var name = 'name:' + $scope.IdName;
  		var address ='{ "name":"'+ $scope.IdName+ '","street":"' +$scope.address+'","number":"'+ $scope.number+'","floor":"' +$scope.floor+'","gate":"' + $scope.dpto+'","province": "C" ,"zipCode":"'+ $scope.postalCode+'","phoneNumber":"'+ $scope.telephone+'"}';
  		$log.debug("http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=CreateAddress&username="+user+"&authentication_token="+token+"&address="+ address);
  		$http.get("http://eiffel.itba.edu.ar/hci/service3/Account.groovy?method=CreateAddress&username="+user+"&authentication_token="+token+"&address="+ address).then(function(res){
