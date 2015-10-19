4480f58a671aa33faf55bf955003c8acb76300fd
nicokuyum <nicokuyum@hotmail.com>
2015-10-19 20:28:26 -0300

    agrega bien al carrito desde wishlist

--- js/wishlist.js
+++ js/wishlist.js
@@ -40,23 +40,23 @@ app.directive('navBar',function(){
 			$scope.emptyList = true;
 		}
 	};
-	this.addToCart = function(id,index){
+	this.addToCart = function(id,index,idForRemove){
 	    $scope.loadingCart = true;
 	    if(ReadCookie("carritoOrderId")==null){
 	      $http.get("http://eiffel.itba.edu.ar/hci/service3/Order.groovy?method=CreateOrder&username=" + user + "&authentication_token=" + token).then(function(res){
 	        $log.debug(res);
 	        document.cookie="carritoOrderId=" + res.data.order.id + "; path=/";
-	        this.addCartWithOrder(id,index);
+	        this.addCartWithOrder(id,index,idForRemove);
 	      });
 	    }else{
-	      this.addCartWithOrder(id,index);
+	      this.addCartWithOrder(id,index,idForRemove);
 	    }
   	};
-  	this.addCartWithOrder = function(id,index){
+  	this.addCartWithOrder = function(id,index,idForRemove){
 	    var orderID = ReadCookie("carritoOrderId");
 	    var esto = this;
 	    $http.get('http://eiffel.itba.edu.ar/hci/service3/Order.groovy?method=AddItemToOrder&username=' + user + '&authentication_token=' + token + '&order_item={"order":{"id":' + orderID + '},"product":{"id": ' + id + '},"quantity":'+ 1 +'}').then(function(res){
-	    	esto.remove(id,index);
+	    	esto.remove(idForRemove,index);
 	    });
   	};
 });
